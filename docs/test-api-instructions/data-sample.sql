b6c9e78ec6ed6d62af86cfdaeebb6758a62fcfce
skcc-wesley <20073916@skcc.com>
2025-05-15 10:40:43 +0700

    update biztask

--- docs/test-api-instructions/data-sample.sql
+++ docs/test-api-instructions/data-sample.sql
@@ -20,16 +20,14 @@ INSERT INTO OCO50200 (
     PHNO, FAX_NO, BZNO, BZMAN_ACQ_DT, TXOFC_CD, TXOFC_NM, COM_BSCND_NM,
     COM_ITM_NM, COM_CORP_NM, USE_YN, CCNTR_CD, RPRTT_NM
 ) VALUES
-('D001', 'system', NOW(), 'D000', 'Tech Support', 'Tech Support', 'TS', '01', 'D000', '1', '20230414',
+('D001', 'system', NOW(), 'D000', 'Tech Support', 'Tech Support', 'TS', '01', null, '1', '20230414',
  '20230414', 'F001', '001', 'S001', 'INV001', 'WH001', 'VAT001', '100000', '123 Tech St', '456 Details Ave',
  '0010', '123-456-7890', '123-456-7891', '1234567890', '20230414', 'TX1', 'Tech Office', 'Tech Company',
  'Item A', 'Corp A', 'Y', 'CN001', 'Report A'),
-
 ('D002', 'system', NOW(), 'D000', 'Sales Team', 'Sales Team', 'ST', '02', 'D001', '2', '20230414',
  '20230414', 'F002', '002', 'S002', 'INV002', 'WH002', 'VAT002', '200000', '789 Sales Rd', '101 Details St',
  '0011', '234-567-8901', '234-567-8902', '2345678901', '20230414', 'TX2', 'Sales Office', 'Sales Company',
  'Item B', 'Corp B', 'Y', 'CN002', 'Report B'),
-
 ('D003', 'system', NOW(), 'D000', 'R&D', 'Research and Development', 'RD', '03', 'D002', '3', '20230414',
  '20230414', 'F003', '003', 'S003', 'INV003', 'WH003', 'VAT003', '300000', '456 Research Ave', '789 Details Blvd',
  '0012', '345-678-9012', '345-678-9013', '3456789012', '20230414', 'TX3', 'R&D Office', 'R&D Company',
@@ -44,22 +42,45 @@ INSERT INTO OCO20101 (
 ('REOFO_CD1', '00', 'Engineer', 2, 'system', NOW());
 
 INSERT INTO OCO50100 (
+  EMPNO, LAST_CHNGR_ID, EMP_KRN_NM, EMP_ENGLNM, EMP_ENGFNM,
+  DEPTCD, BTHDY, OWHM_PHNO, MPHNO,
+  GROUP_ENTCP_DT, TCOM_ENTCP_DT, CURTP_OFORD_DT, CURTP_OFORD_CD,
+  RESG_DT, COM_CL_CD, CLOFP_CD, REOFO_CD, JGP_CD, VCTN_CD,
+  TEAMBR_CL_CD, CLOFP_NM, JGP_NM, VCTN_NM,
+  EMAILADDR, BEFO_EMPNO
+)
+VALUES (
+  'E100001', 'SYSTEM', 'Kim Minsoo', 'Kim', 'Minsoo',
+  'D002', '19900101', '010-1111-2222', '010-2222-3333',
+  '20150101', '20150101', '20220101', '001',
+  NULL, 'A', '001', '200', 'B', '003',
+  'A01', 'Manager', 'Engineering', 'Frontend',
+  'minsoo.kim@example.com', NULL
+);
+
+INSERT INTO OCO50100 (
     EMPNO, EMP_KRN_NM, EMP_ENGLNM, EMP_ENGFNM, DEPTCD, CLOFP_NM, VCTN_NM,
     LAST_CHNGR_ID, LAST_CHNG_DTMD, OWHM_PHNO, GROUP_ENTCP_DT, TCOM_ENTCP_DT,
     CURTP_OFORD_DT, CURTP_OFORD_CD, COM_CL_CD, CLOFP_CD, REOFO_CD, JGP_CD,
     VCTN_CD, TEAMBR_CL_CD, JGP_NM, EMAILADDR, MPHNO, RESG_DT
 ) VALUES
 (
+    'E0003', 'John', 'Smith T', 'John T', 'D003', 'Senior', 'IT',
+    'system', NOW(), '123-456-7890', '20240414', '20240415',
+    '20240416', 'C01', 'A', 'B01', '112', 'R', 'J',
+    'V01', 'T01', 'J', '123-456-7890', NULL
+),
+(
     'E0001', 'John Smith', 'Smith', 'John', 'D001', 'Senior', 'IT',
     'system', NOW(), '123-456-7890', '20240414', '20240415',
-    '20240416', 'C01', 'A', 'B01', 'C01', 'R', 'J',
-    'V01', 'T01', 'J', 'john.smith', NULL
+    '20240416', 'C01', 'A', 'B01', '116', 'R', 'J',
+    'V01', 'T01', 'J', '123-456-7890', NULL
 ),
 (
     'E0002', 'Alice Brown', 'Brown', 'Alice', 'D002', 'Manager', 'Sales',
     'system', NOW(), '234-567-8901', '20240414', '20240415',
-    '20240418', 'C02', 'B', 'B02', 'C02', 'R', 'J',
-    'V02', 'T02', 'J', 'alice.brown', NULL
+    '20240418', 'C02', 'B', 'B02', '200', 'R', 'J',
+    'V02', 'T02', 'J', '123-456-7899', NULL
 );
 
 INSERT INTO OCO10100 (
