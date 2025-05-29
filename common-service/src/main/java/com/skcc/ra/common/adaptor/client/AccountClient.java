7683ce55e201af2f84a90fa9ca4813b3d59d9399
skcc-tom01 <20073917@skcc.com>
2025-05-29 12:58:45 +0700

    fix bug wrong url

--- common-service/src/main/java/com/skcc/ra/common/adaptor/client/AccountClient.java
+++ common-service/src/main/java/com/skcc/ra/common/adaptor/client/AccountClient.java
@@ -7,11 +7,11 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "account-service", url = "${feign.account-service.url}", configuration = {FeignConfig.class})
+@FeignClient(name = "account-service", url = "${feign.account-service.url:http://localhost:9101}", configuration = {FeignConfig.class})
 public interface AccountClient {
 
     @Schema(name="ID로 계정조회")
-    @GetMapping("/api/v1/com/account/userid")
+    @GetMapping("/api/v1/com/account/user/userid")
     AccountDto searchAccountByUserid(@RequestParam(required = false, value = "userid") String userid);
 
 }
